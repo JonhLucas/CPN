0d0558268f03e021f2d5448a3c92f18770003d7e
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-10-24 10:01:46 -0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,155 +1,65 @@
-def ida(node, l1, l2):
-    x = (node[1] > node[0] & node[0] == 0)
-    y = (node[1] <= node[0])
+def validade(p):
+    g = p[0] >= p[1]
+    e = (p[0] < p[1]) and (p[0] == 0)
+    g = g or e
+    x = p[2] >= p[3]
+    y = (p[2] < p[3]) and (p[2] == 0)
     x = x or y
-    a = (node[4] > node[3] & node[3] == 0)
-    b = (node[4] <= node[3])
-    a = a or b
-    c = node not in l1
-    print(x and a, l1[0] != node, node not in l2,"ida: ", node)
-    if x and a:
-        new = node.copy()
-        if new[0] >= 2:
-            new[0] -= 2
-            new[2] = new[0] + new[1]
-            new[3] += 2
-            new[5] = new[4] + new[3]
-            if new not in l1:
-                lista.append(new)
-                l1.append(new)
-                print("inserido", new)
-                volta(new, l1, l2)
-        new = node.copy()
-        if new[1] >= 2:
-            new[1] -= 2
-            new[2] = new[0] + new[1]
-            new[4] += 2
-            new[5] = new[4] + new[3]
-            if new not in l1:
-                lista.append(new)
-                l1.append(new)
-                print("inserido", new)
-                #print(new)
-                volta(new, l1, l2)
-        new = node.copy()
-        if new[1] >= 1 & new[0] >= 1:
-            new[0] -= 1
-            new[1] -= 1
-            new[2] = new[0] + new[1]
-            new[3] += 1
-            new[4] += 1
-            new[5] = new[3] + new[4]
-            if new not in l1:
-                lista.append(new)
-                l1.append(new)
-                print("inserido", new)
-                #print(new)
-                volta(new, l1, l2)
-        new = node.copy()
-        if new[0] >= 1:
-            new[0] -= 1
-            new[2] = new[0] + new[1]
-            new[3] += 1
-            new[5] = new[4] + new[3]
-            if new not in l1:
-                lista.append(new)
-                l1.append(new)
-                print("inserido", new)
-                #print(new)
-                volta(new, l1, l2)
-        new = node.copy()
-        if new[1] >= 1:
-            new[1] -= 1
-            new[2] = new[0] + new[1]
-            new[4] += 1
-            new[5] = new[4] + new[3]
-            if new not in l1:
-                lista.append(new)
-                l1.append(new)
-                print("inserido", new)
-                #print(new)
-                volta(new, l1, l2)
+    return g and x
+
+
+def vitoria(p):
+    return p == [0,0,3,3]
+def ida(place, l1, l2, keep):
+    if validade(place):
+        print("lado esquerdo", place)
+        for i in range(3):
+            for j in range(3 - i):
+                if (i or j) and place[0] >= i and place[1] >= j:
+                    new_node = place.copy()
+                    new_node[0] -= i
+                    new_node[1] -= j
+                    new_node[2] += i
+                    new_node[3] += j
+                    if new_node not in l2 and keep:
+                        #history.append(new_node)
+                        l2.append(new_node)
+                        print("     incluir em l2:",new_node)
+                        volta(new_node, l1, l2, keep)
     else:
-        print("recusado ida: ", node)
+        print("recusado no lado esquerdo:", place)
 
-def volta(no, l1, l2):
-    x =(no[4] > no[3] & no[3] == 0)
-    y = (no[4] <= no[3])
-    x = x or y
-    a = (no[1] > no[0] & no[0] == 0)
-    b = (no[1] <= no[0])
-    a = a or b
-    print(x and a, "volta: ", no)
-    if x and a:
-        new_node = no.copy()
-        if new_node[3] >= 1:
-            new_node[3] -= 1
-            new_node[5] -= 1
-            new_node[0] += 1
-            new_node[2] += 1
-            if new_node not in l2:
-                lista.append(new_node)
-                l2.append(new_node)
-                print("--- ",new_node)
-                ida(new_node, l1, l2)
-        new_node = no.copy()
-        if new_node[3] >= 2:
-            new_node[3] -= 2
-            new_node[5] -= 2
-            new_node[0] += 2
-            new_node[2] += 2
-            if new_node not in l2:
-                lista.append(new_node)
-                l2.append(new_node)
-                print("--- ",new_node)
-                ida(new_node, l1, l2)
-        new_node = no.copy()
-        if new_node[4] >= 1:
-            new_node[4] -= 1
-            new_node[5] -= 1
-            new_node[1] += 1
-            new_node[2] += 1
-            if new_node not in l2:
-                lista.append(new_node)
-                l2.append(new_node)
-                print("--- ",new_node)
-                ida(new_node, l1, l2)
-        new_node = no.copy()
-        if new_node[4] >= 2:
-            new_node[4] -= 2
-            new_node[5] -= 2
-            new_node[1] += 2
-            new_node[2] += 2
-            if new_node not in l2:
-                lista.append(new_node)
-                l2.append(new_node)
-                print("--- ",new_node)
-                ida(new_node, l1, l2)
-        new_node = no.copy()
-        if new_node[3] >= 1 & new_node[4] >= 1:
-            new_node[3] -= 1
-            new_node[4] -= 1
-            new_node[5] -= 2
-            new_node[0] += 1
-            new_node[1] += 1
-            new_node[2] += 2
-            if new_node not in l2:
-                lista.append(new_node)
-                l2.append(new_node)
-                print("--- ", new_node)
-                ida(new_node, l1, l2)
-            else:
-                print("erro")
-        print("fim")
+
+def volta(place, l1, l2, keep2):
+    if vitoria(place):
+        keep2 = False
+        print("                         Achou")
+    elif validade(place):
+        print("lado direito", place)
+        for i in range(3):
+            for j in range(3 - i):
+                if (i or j) and place[2] >= i and place[3] >= j:
+                    new_node = place.copy()
+                    new_node[0] += i
+                    new_node[1] += j
+                    new_node[2] -= i
+                    new_node[3] -= j
+                    print(i,j,":", new_node)
+                    if new_node not in l1 and keep2:
+                        #history.append(new_node)
+                        l1.append(new_node)
+                        print("     incluir em l1:", new_node)
+                        ida(new_node,l1,l2, keep2)
     else:
-        print("recusado volta: ", no)
+        print("recusado no lado esquerdo:", place)
 
 
-no = [3, 3, 6, 0, 0, 0]
-lista = [no]
-l1 = [no]
+root = [3, 3, 0, 0]
+history = []
+l1 = [root]
 l2 = []
-ida(lista[0], l1, l2)
-print(lista)
-print(l1)
-print(l2)
+keeper = True
+ida(root, l1, l2, keeper)
+print("Lado esquerdo:", l1)
+print("Lado direito:",l2)
+

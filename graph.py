72a98a6073219ca5250359209e579fcaf4b3e312
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-11-24 22:45:44 -0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ def validate(p):
     return (p[0] >= p[1] or (p[0] < p[1] and p[0] == 0)) and (p[2] >= p[3] or (p[2] < p[3] and p[2] == 0))
 
 
-def ida(place, list1, list2, index):
+def left_to_right(place, list1, list2, index):
     local = []
     for i in range(3):
         for j in range(3 - i):
@@ -20,10 +20,10 @@ def ida(place, list1, list2, index):
     while len(local) != 0 and [0, 0, 3, 3] not in list2:
         node = local.pop(0)
         history.append(node + [index])
-        volta(node, list1, list2, len(history)-1)
+        right_to_left(node, list1, list2, len(history) - 1)
 
 
-def volta(place, li1, li2, index):
+def right_to_left(place, li1, li2, index):
     local = []
     for i in range(3):
         for j in range(3 - i):
@@ -39,14 +39,14 @@ def volta(place, li1, li2, index):
     while len(local) != 0 and [0, 0, 3, 3] not in li2:
         node = local.pop(0)
         history.append(node + [index])
-        ida(node, li1, li2, len(history) - 1)
+        left_to_right(node, li1, li2, len(history) - 1)
 
 
 root = [3, 3, 0, 0]
 history = [root+[-1]]
 l1 = [root]
 l2 = []
-ida(root, l1, l2, 0)
+left_to_right(root, l1, l2, 0)
 begin = history[-1]
 path = []
 while begin[4] != -1:

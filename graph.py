a940a8eb56d8294296006a91ea5c5ef929420075
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-11-24 22:39:00 -0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,68 +1,57 @@
-keeper = True
+def validate(p):
+    return (p[0] >= p[1] or (p[0] < p[1] and p[0] == 0)) and (p[2] >= p[3] or (p[2] < p[3] and p[2] == 0))
 
 
-def validade(p):
-    g = p[0] >= p[1]
-    e = (p[0] < p[1]) and (p[0] == 0)
-    g = g or e
-    x = p[2] >= p[3]
-    y = (p[2] < p[3]) and (p[2] == 0)
-    x = x or y
-    return g and x
+def ida(place, list1, list2, index):
+    local = []
+    for i in range(3):
+        for j in range(3 - i):
+            if (i or j) and place[0] >= i and place[1] >= j:
+                new_node = place.copy()
+                new_node[0] -= i
+                new_node[1] -= j
+                new_node[2] += i
+                new_node[3] += j
+                if new_node not in list2 and validate(new_node) and [0, 0, 3, 3] not in list2:
+                    local.append(new_node)
+                    list2.append(new_node)
+                    if new_node == [0, 0, 3, 3]:
+                        history.append([0, 0, 3, 3, index])
+    while len(local) != 0 and [0, 0, 3, 3] not in list2:
+        node = local.pop(0)
+        history.append(node + [index])
+        volta(node, list1, list2, len(history)-1)
 
 
-def vitoria(p):
-    return p == [0, 0, 3, 3]
-
-
-def ida(place, l1, l2):
-    if validade(place):
-        local = []
-        print("lado esquerdo", place)
-        for i in range(3):
-            for j in range(3 - i):
-                if (i or j) and place[0] >= i and place[1] >= j:
-                    new_node = place.copy()
-                    new_node[0] -= i
-                    new_node[1] -= j
-                    new_node[2] += i
-                    new_node[3] += j
-                    if new_node not in l2 and [0, 0, 3, 3] not in l2:
-                        local.append(new_node)
-                        l2.append(new_node)
-        while len(local) != 0 and [0, 0, 3, 3] not in l2:
-            volta(local.pop(0), l1, l2)
-    else:
-        print("recusado no lado esquerdo:", place)
-
-
-def volta(place, l1, l2):
-    if vitoria(place):
-        print("                         Achou")
-    elif validade(place):
-        local = []
-        print("lado direito", place)
-        for i in range(3):
-            for j in range(3 - i):
-                if (i or j) and place[2] >= i and place[3] >= j:
-                    new_node = place.copy()
-                    new_node[0] += i
-                    new_node[1] += j
-                    new_node[2] -= i
-                    new_node[3] -= j
-                    if new_node not in l1:
-                        local.append(new_node)
-                        l1.append(new_node)
-        while len(local) != 0 and [0, 0, 3, 3] not in l2:
-            ida(local.pop(0), l1, l2)
-    else:
-        print("recusado no lado esquerdo:", place)
+def volta(place, li1, li2, index):
+    local = []
+    for i in range(3):
+        for j in range(3 - i):
+            if (i or j) and place[2] >= i and place[3] >= j:
+                new_node = place.copy()
+                new_node[0] += i
+                new_node[1] += j
+                new_node[2] -= i
+                new_node[3] -= j
+                if new_node not in li1 and validate(new_node):
+                    local.append(new_node)
+                    li1.append(new_node)
+    while len(local) != 0 and [0, 0, 3, 3] not in li2:
+        node = local.pop(0)
+        history.append(node + [index])
+        ida(node, li1, li2, len(history) - 1)
 
 
 root = [3, 3, 0, 0]
-history = []
+history = [root+[-1]]
 l1 = [root]
 l2 = []
-ida(root, l1, l2)
-print("*Lado esquerdo:", l1)
-print("*Lado direito:", l2)
+ida(root, l1, l2, 0)
+begin = history[-1]
+path = []
+while begin[4] != -1:
+    path.append(begin)
+    begin = history[begin[4]]
+path.append(root+[-1])
+path.reverse()
+print(path)
